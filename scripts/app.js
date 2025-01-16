2eb256f6e94a3225a99b530e8df23a369a0a9113
Jatin-Bagga1 <156444622+Jatin-Bagga1@users.noreply.github.com>
2025-01-16 18:39:16 -0500

    second commit

--- scripts/app.js
+++ scripts/app.js
@@ -12,6 +12,32 @@
         {
             location.href = "about.html";
         });
+
+        let MainContent = document.getElementsByTagName("main")[0];
+        let MainParagraph = document.createElement("p");
+
+        MainParagraph.setAttribute("id", "MainParagraph");
+        MainParagraph.setAttribute("class","mt-3");
+        MainParagraph.textContent = "This is my first main paragraph";
+
+        MainContent.appendChild(MainParagraph);
+
+        let FirstString = "This is";
+        // Back tick is used to create string literal.
+        // Concatinating two string values
+        let SecondString = `${FirstString} my second string`;
+
+        MainParagraph.textContent = SecondString;
+        MainContent.appendChild(MainParagraph);
+
+        // Accessing whole DOM of index.html
+        let DocumentBody = document.body;
+        let Article = document.createElement("article");
+        let ArticleParagraph = `<p id="ArticleParagraph" class="mt-3">This is my first article paragraph</p>`;
+        Article.setAttribute("class" , "container");
+        Article.innerHTML = ArticleParagraph;
+        DocumentBody.appendChild(Article);
+
     }
     function DisplayProductPage()
     {
